8e863d55357b1bf88c0ba77f7dba730db9fa0ccd
JadeWongyoungyoo <nawaf.w1@gmail.com>
2021-03-28 22:07:04 +0700

    ส่งข้อสอบข้อ 2

---
+++ Test ข้อ 2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Console_redy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x;
+            int y;
+            x = a;
+            y = b;
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+
+            }
+            Console.Write(x);
+
+        }
+    }
+}
